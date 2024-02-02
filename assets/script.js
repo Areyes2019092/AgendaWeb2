0b48dc7c109bc2db70204a8e6896cfad57d272d1
Angel <areyes-2019092@kinal.edu.gt>
2024-02-01 18:12:10 -0600

    Agregue el index

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+/*
+function login() {
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+
+    if (email === "usuario@example.com" && phone === "1234567890") {
+        alert("¡Inicio de sesión exitoso!");
+    } else {
+        alert("Inicio de sesión fallido. Verifica tus credenciales.");
+    }
+}
+*/
